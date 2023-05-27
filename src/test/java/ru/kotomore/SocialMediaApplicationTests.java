c35f387c93b412a1cf51630715db03722d4acdc5
set404 <89992008819@mail.ru>
2023-05-27 13:23:38 +0300

    Initial commit

---
+++ src/test/java/ru/kotomore/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kotomore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
